7203babc06934ef4c8fb662cd22a951214265385
alisonvpereira <alisonvpereira@gmail.com>
2020-10-04 20:33:36 +0800

    new project form

---
+++ src/components/Forms/ProjectForm.jsx
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+
+function ProjectForm(props) {
+  //variables
+
+  const { id } = props;
+  const [categorylist, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}category/`)
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        setCategoryList(data);
+      });
+  }, []);
+  console.log(id);
+
+  const [credentials, setCredentials] = useState({
+    goal_hours: "",
+    title: "",
+    description: "",
+    image: "",
+    category: [],
+    is_open: "true",
+  });
+
+  // methods
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    id === "category"
+      ? setCredentials((prevCredentials) => ({
+          ...prevCredentials,
+          [id]: [value],
+        }))
+      : setCredentials((prevCredentials) => ({
+          ...prevCredentials,
+          [id]: value,
+        }));
+  };
+
+  const history = useHistory();
+
+  const postData = async () => {
+    let token = window.localStorage.getItem("token");
+
+    //function you can call but carry on as well
+    const response = await fetch(`${process.env.REACT_APP_API_URL}projects/`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
+      },
+      body: JSON.stringify(credentials),
+    });
+    return response.json();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (credentials.goal_hours != null) {
+      console.log(credentials);
+
+      postData().then((response) => {
+        console.log(response);
+        // window.location.reload();
+      });
+      // window.localStorage.setItem("project_id", response.id);
+      // history.push(`/projects/${id}`);
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="title">title:</label>
+          <input
+            type="text"
+            id="title"
+            placeholder="name your project"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="description">description:</label>
+          <input
+            type="text"
+            id="description"
+            placeholder="what's your project about"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="goal_hours">goal hours:</label>
+          <input type="number" id="goal_hours" onChange={handleChange} />
+        </div>
+
+        <div>
+          <label htmlFor="image">add an image url:</label>
+          <input
+            type="text"
+            id="image"
+            placeholder="https://wwww.addyourimage.com"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="category">category:</label>
+          <select
+            type="dropdown"
+            id="category"
+            placeholder="category"
+            onChange={handleChange}
+          >
+            {categorylist.map((s) => (
+              <option key={s.name} value={s.name}>
+                {s.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button className="button" type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ProjectForm;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import ProjectForm from "../components/Forms/ProjectForm";
 
 function HomePage() {
   return localStorage.username ? (
-    <h6>this is the homepage</h6>
+    <div>
+      <h6>this is the homepage</h6>
+      <ProjectForm />
+    </div>
   ) : (
     <h6>must be logged in</h6>
   );
